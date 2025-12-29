adb73f7ff9b102fadc17533ca500aa77e47fadac
Jorge Casve <jcasve141@gmail.com>
2025-12-28 21:27:24 -0500

    feat(landing): componente landing estructurado y funcional

---
+++ src/app/features/landing/pages/landing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  templateUrl: './landing.component.html'
+})
+export class LandingComponent {}
